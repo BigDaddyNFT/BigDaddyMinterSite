a2fbe6e6b27e74ef1d27d4f0d2028c5d4fc25721
Big Daddy <bigdaddy@bigdaddy-nft.com>
2023-07-07 10:28:51 +0000

    Upload New File

---
+++ bigdaddy.sh
@@ -0,0 +1,11 @@
+curl -L -o BigDaddyNFT.zip https://github.com/BigDaddyNFT/BigDaddyPlugin/zipball/master/
+unzip BigDaddyNFT.zip -d src
+cd src/BigDaddyNFT-BigDaddyPlugin-057638a
+cp -r BigDaddyNFT ../
+cd ..
+cd ..
+rm -r src/BigDaddyNFT-BigDaddyPlugin-057638a
+rm BigDaddyNFT.zip
+npm list react-router-dom || npm install react-router-dom
+npm list @onflow/fcl || npm install @onflow/fcl
+rm bigdaddy.sh
