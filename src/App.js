dddf94970be87955b6793103b98e69d7924737a1
Big Daddy <bigdaddy@bigdaddy-nft.com>
2023-07-07 12:27:46 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { BigDaddyProvider } from './Provider/BigDaddyMinterContext';
 import "./flow/config";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import BigDaddyMinterComponent from './Components/BigDaddyMinterComponent';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter basename="/" >
       <BigDaddyProvider>
         <Routes>
           <Route path="/" element={<BigDaddyMinterComponent />} />
         </Routes>
       </BigDaddyProvider>
-    </Router>
+    </HashRouter>
   );
 }
 
