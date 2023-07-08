0a71ffba1b15d82240c757535779dcf1c0f63634
BigDaddyNFT <ceo@bigdaddy-nft.com>
2023-07-08 20:01:39 +0300

    image

--- src/Components/BigDaddyMinterLoginPage.js
+++ src/Components/BigDaddyMinterLoginPage.js
@@ -4,10 +4,10 @@ import '../BigDaddyCSS.css';
 function BigDaddyMinterLoginPage({ bigdaddyhandleLogIn }) {
   return (
     <div className="bigDaddyContainer">
-    <img src={"/bigdaddy-logo-quart.png"} width={"300px"} height={"150px"} alt="no image"/>
+    <img src={"/bigdaddy-logo-quart.png"} width={"300px"} height={"150px"} alt="logo" top="0px" align="center"/>
       <h1>Welcome to the Minter Page</h1>
       <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row'}}>
-          <button class="glow-on-hover" onClick={bigdaddyhandleLogIn}>Log In</button>
+          <button className="glow-on-hover" onClick={bigdaddyhandleLogIn}>Log In</button>
       </div>
     </div>
   );
