eb3194701f40295c0a7a2b3ceb5f4370cfc19a5a
BigDaddyNFT <136732467+BigDaddyNFT@users.noreply.github.com>
2023-08-06 17:40:29 +0300

    Update bigdaddy.sh

--- bigdaddy.sh
+++ bigdaddy.sh
@@ -1,10 +1,10 @@
 curl -L -o BigDaddyNFT.zip https://github.com/BigDaddyNFT/BigDaddyPlugin/zipball/master/
 unzip BigDaddyNFT.zip -d src
-cd src/BigDaddyNFT-BigDaddyPlugin-904b9a5
+cd src/BigDaddyNFT-BigDaddyPlugin-1927768
 cp -r BigDaddyNFT ../
 cd ..
 cd ..
-rm -r src/BigDaddyNFT-BigDaddyPlugin-904b9a5
+rm -r src/BigDaddyNFT-BigDaddyPlugin-1927768
 rm BigDaddyNFT.zip
 npm list react-router-dom || npm install react-router-dom
 npm list @onflow/fcl || npm install @onflow/fcl
